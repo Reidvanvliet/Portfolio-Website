3f7ede2799c0e0eae4874c072f8f8aa41bca84c1
Reid Vanvliet <Reidvanvliet56@hotmail.com>
2025-04-15 12:10:30 -0700

    added websites to pojects, made contact button, made offers page.

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -1,9 +1,36 @@
 let me = document.getElementById("imageMe");
+let contact = document.getElementById("contact");
+let close = document.getElementById('close');
+const words = new URLSearchParams(window.location.search);
+const message = words.get("message");
+const messageEl = document.getElementById('offers');
+
+function contactMe () {
+    document.getElementById('contactMe').style.display = 'block';
+}
+
+function contactMeClose () {
+    document.getElementById('contactMe').style.display = 'none';
+}
+
 function disappear () {
     me.style.display = 'none';
 }
+
 function reappear () {
     me.style.display = "block";
 }
-me.onmouseover = disappear;
-me.onmouseout = reappear;
+
+if (me != null){
+    me.onmouseover = disappear;
+}
+
+if (me != null) {
+    me.onmouseout = reappear;
+}
+
+if (messageEl != null) {
+    messageEl.innerHTML = message;
+}
+contact.onclick = contactMe;
+close.onclick = contactMeClose;
